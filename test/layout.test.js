04d0189be5bf78d48b407ce2ea4f2be5471c4932
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2019-01-10 15:46:48 +0530

    Add enzyme test

---
+++ test/layout.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import Layout from '../src/layout.jsx';
+
+
+spy(Layout.prototype, 'render');
+
+describe('<Layout />', () => {
+  it('calls render', () => {
+    const wrapper = shallow(<Layout />);
+    expect(Layout.prototype.render).to.have.property('callCount', 1);
+  });
+});
