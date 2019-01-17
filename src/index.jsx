5b81ede4d49823d0dbef090994cea634f54cd97a
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2019-01-17 16:01:03 +0530

    Add css loader

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Layout from './layout.jsx';
+import Layout from 'layout.jsx';
+import "css/index.css"
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Layout />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require("path");
 var webpack = require("webpack");
-
+var ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
 var ROOT = path.resolve(__dirname);
 var SRC_DIR = path.resolve(__dirname, "src");
 
@@ -19,9 +19,19 @@ config = {
         use: {
           loader: "babel-loader"
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextWebpackPlugin.extract({
+          use: "css-loader",
+        }),
+      },
     ]
-  }
+  },
+  resolve: {
+    modules: [path.resolve("./src"), path.resolve("./node_modules")]
+  },
+  plugins: [new ExtractTextWebpackPlugin("index.css")]
 };
 
 module.exports = [config];
